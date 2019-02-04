ef84034c3d00db3ef8ccf315174bc3d3346b8e1a
Changyu Yan <changyannnnnn@gmail.com>
2019-02-04 02:12:10 -0800

    added

---
+++ Roulette/Roulette.py
@@ -0,0 +1,33 @@
+import random
+
+class FairRoulette():
+    def __init__(self):
+        self.pockets = list(range(1, 37))
+        self.ball = None
+        self.pocketOdds = len(self.pockets) - 1
+    def spin(self):
+        self.ball = random.choice(self.pockets)
+    def betPocket(self, pocket, amt):
+        if str(pocket) == str(self.ball):
+            return amt * self.pocketOdds
+        else:
+            return -amt
+    def __str__(self):
+        return "Fair Roulette"
+    
+def playRoulette(game, numSpins, pocket, bet, toPrint): 
+    totPocket = 0 
+    for i in range(numSpins):
+        game.spin() 
+        totPocket += game.betPocket(pocket, bet)
+    if toPrint: 
+        print(numSpins, 'spins of', game) 
+        print('Expected return betting', pocket, '=', str(100*totPocket/numSpins) + '%\n') 
+    return (totPocket / numSpins)
+
+
+game = FairRoulette()
+
+for numSpins in (100, 1000000): 
+    for i in range(3): 
+        playRoulette(game, numSpins, 2, 1, True)
